d086d1fdf4a5d2bc1265937ab9319878c8ab9d0b
laserbeam2045 <laserbeam2045@gmail.com>
2025-09-17 11:47:59 +0900

    fix: 文言

--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -18,8 +18,7 @@ const HomeScreen: React.FC<HomeScreenProps> = ({
         頭脳の限界を超えろ！
       </h1>
       <p className="text-xl text-gray-300 max-w-3xl mx-auto mb-8">
-        様々なカテゴリーの問題に挑戦して、リドルマスターの称号を手に入れよう！
-        複数のパズルゲームも追加されたぞ！
+        様々な種類の問題に挑戦して、あなたの知的好奇心を満たそう！
       </p>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 max-w-2xl">
         {/* ボタンをグリッドレイアウトに変更 */}
@@ -33,19 +32,19 @@ const HomeScreen: React.FC<HomeScreenProps> = ({
           className="bg-gradient-to-r from-cyan-500 to-blue-500 text-white py-3 px-8 rounded-full font-bold uppercase tracking-wider shadow-lg hover:shadow-xl transform hover:-translate-y-1 transition-all duration-300"
           onClick={onStartPuzzle}
         >
-          パズルをプレイ
-        </button>
-        <button
-          className="bg-gradient-to-r from-orange-500 to-red-500 text-white py-3 px-8 rounded-full font-bold uppercase tracking-wider shadow-lg hover:shadow-xl transform hover:-translate-y-1 transition-all duration-300"
-          onClick={onStartSlidingPuzzle}
-        >
-          スライディングパズル
+          繋げるパズルをプレイ
         </button>
         <Link href="/catgame" passHref>
           <button className="bg-gradient-to-r from-green-400 to-teal-500 text-white py-3 px-8 rounded-full font-bold uppercase tracking-wider shadow-lg hover:shadow-xl transform hover:-translate-y-1 transition-all duration-300">
             猫と鼠をプレイ
           </button>
         </Link>
+        <button
+          className="bg-gradient-to-r from-orange-500 to-red-500 text-white py-3 px-8 rounded-full font-bold uppercase tracking-wider shadow-lg hover:shadow-xl transform hover:-translate-y-1 transition-all duration-300"
+          onClick={onStartSlidingPuzzle}
+        >
+          スライディングパズル
+        </button>
       </div>
     </div>
   );
